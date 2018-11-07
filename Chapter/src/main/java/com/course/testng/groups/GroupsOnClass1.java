c6c802d4fcdfec8742574d592787f5f7bdbaf94f
apple <361914192@qq.com>
2018-11-07 11:39:05 +0800

    组测试中的类的分组

---
+++ Chapter/src/main/java/com/course/testng/groups/GroupsOnClass1.java
@@ -0,0 +1,10 @@
+package com.course.testng.groups;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "stu")
+public class GroupsOnClass1 {
+    public void stu1(){
+        System.out.println("GroupsOnClass1中的stu1运行");
+    }
+}
